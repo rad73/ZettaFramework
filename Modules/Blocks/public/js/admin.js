ce1ddbde4b69d7f5436e35ad1ebc486c345eaf62
Александр Хрищанович <dirmax@yandex.ru>
2016-02-18 23:22:42 +0300

    Add helper ->block({name}, 'text') this is only plain-text editable block

--- Modules/Blocks/public/js/admin.js
+++ Modules/Blocks/public/js/admin.js
@@ -1,4 +1,4 @@
-$(function() { 
+$(function() {
 
 	$('input[name=z_block_switch]').change(function () {
 
@@ -12,7 +12,7 @@ $(function() {
 		}
 
 	});
-	
+
 	if ($.cookie('z_blocks_enabled')) {
 		$('input[name=z_block_switch]').each(function () {
 			if ($(this).val() == 'on') {
@@ -28,61 +28,61 @@ $(function() {
 		})
 	}
 
-	
+
 });
 
 var _initBlocks = function () {
-	
+
 	var in_edit = false;
 
 	_destroyBlocks();
 
 	$('#z_blocks_wrapper').removeClass('z_blocks_disabled');
-	
+
 	$('.icon-pencil, .icon-lock, .icon-unlock').click(function () {
-		
+
 		var _parent = $(this).parents('.z_admin_wrapper:first');
 			_text = $('.z_redactor', _parent);
-			
+
 		if (_redactor._object && _redactor._object.get(0) == _text.get(0)) {
 			in_edit = true;
 		}
 		else {
 			in_edit = false;
 		}
-		
+
 	})
-	
+
 	/* Иконка редактирования блока */
 	$('.icon-pencil').click(function () {
-		
+
 		if (in_edit) return false;
-		
+
 		var _parent = $(this).parents('.z_admin_wrapper:first');
 		_createEditor(_parent, _currentRouteId);
-				
+
 		return false;
 
 	});
-	
+
 	/* иконка блока содержимое которое наследуется */
 	$('.icon-lock').click(function () {
-		
+
 		if (in_edit) return false;
-		
+
 		var _parent = $(this).parents('.z_admin_wrapper:first');
-		
+
 		_confirm(
-			'Внимание!', 
-			'Содержимое блока наследуется от главной страницы, заменить текст блока для этой страницы?', 
+			'Внимание!',
+			'Содержимое блока наследуется от главной страницы, заменить текст блока для этой страницы?',
 			function () {
 				_createEditor(_parent, _currentRouteId);
 				_parent
 					.addClass('z_override_block')
 					.removeClass('z_inherit_block');
-			}, 
-			'Заменить', 
-			'Редактировать', 
+			},
+			'Заменить',
+			'Редактировать',
 			function () {
 				_createEditor(_parent, 1);
 				_parent
@@ -90,38 +90,38 @@ var _initBlocks = function () {
 					.removeClass('z_override_block');
 			}
 		);
-		
+
 		return false;
 	});
-	
+
 	/* иконка блока содержимое которое не наследуется */
 	$('.icon-unlock').click(function (e) {
-		
+
 		if (in_edit) return false;
-		
+
 		var _parent = $(this).parents('.z_admin_wrapper:first');
-		
+
 		if (e.pageX) {		// физически кликнули по кнопке - выставляем контент родителя
-			
+
 			_confirm(
-				'Внимание!', 
-				'Наследовать содержимое с главной страницы?', 
+				'Внимание!',
+				'Наследовать содержимое с главной страницы?',
 				function () {
-					
+
 					$.post(_urlBlockInfo, {format: 'json', block_name: _parent.data('block-name'), csrf_hash: _csrf_hash}, function (data) {
-						
+
 						$('.z_redactor', _parent).html(data.block.content);
-						
+
 						_parent
 							.addClass('z_inherit_block')
 							.removeClass('z_override_block');
-							
+
 						$.post(_urlBlockDelete, {format: 'json', block_name: _parent.data('block-name'), route_id: _currentRouteId, csrf_hash: _csrf_hash}, function (data) {
-							
+
 						});
-						
+
 					}, 'json');
-					
+
 				}
 
 			);
@@ -132,46 +132,53 @@ var _initBlocks = function () {
 		}
 		return false;
 	});
-	
-	
+
+
 	/* Клакаем по тексту для редактирования*/
 	$('.z_redactor').click(function () {
 
 		var _parent = $(this).parents('.z_admin_wrapper:first');
 		$('.z_icons_editable VAR:visible', _parent).click();
-		
+
 		return false;
-		
+
 	});
 
 
 	var _createEditor = function(_parent, _route_id) {
-		
+
 		var _this = $('.z_redactor', _parent),
 			_callback = function (html, object) {
-				
+
 				var _block_name = _parent.data('block-name');
-				
+
 				$.post(_urlBlockSave, {format: 'json', block_name: _block_name, content: html, route_id: _route_id, csrf_hash: _csrf_hash}, function () {
-				
+
 				}, 'json');
-				
+
 			}
-		
+
 		_redactor.destroy();
-			
+
 		switch (_parent.data('block-type')) {
 			case 'html':
 				_redactor.html(_this, _callback);
+				_redactor._view._bodyClick({pageX: _parent.offset().left, pageY: _parent.offset().top});
+			break;
+			case 'text':
+				_redactor.html(_this, _callback, {
+					toolbar: false,
+					allowedTags: ['var']
+				});
 				_redactor._view._bodyClick({pageX: _parent.offset().left, pageY: _parent.offset().top});
 			break;
 			case 'image':
 				_redactor.image(_this, _callback);
 				break;
 		}
-		
+
 	}
-	
+
 }
 
 var _destroyBlocks = function () {
