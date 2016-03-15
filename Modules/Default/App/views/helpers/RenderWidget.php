4249f3d1a9561a681497fc3b05df2404ca9a6297
Александр Хрищанович <dirmax@yandex.ru>
2016-03-16 01:38:44 +0300

    Fix right helper URL in RenderWidget helper

--- Modules/Default/App/views/helpers/RenderWidget.php
+++ Modules/Default/App/views/helpers/RenderWidget.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * Переписанный action helper 
+ * Переписанный action helper
  * Теперь вызывать можно любой модуль, а не только тот в котором находится текущая обработка
  *
  */
 class Zetta_View_Helper_RenderWidget extends Zend_View_Helper_Action {
-	
+
 	protected static $_COUNTER = 1;
-    
+
     public function renderWidget($scriptPath, $widgetPath, $params = array()) {
-    	
+
     	$view = $this->cloneView();
-    	
+
     	if (sizeof($params)) {
     		foreach ($params as $k=>$v) {
 		    	$view->$k = $v;
     		}
     	}
 
-		$view->setBasePath($scriptPath);
+		$view->addBasePath($scriptPath);
 
 		$this->view->placeholder('z_panel_modules')->{self::$_COUNTER} = $view->render($widgetPath);
 		self::$_COUNTER++;
-        
+
     }
 
 }
