58c3bcd91cbddf54bf6bb34ac7def36536f1cac7
Александр Хрищанович <dirmax@yandex.ru>
2016-04-19 18:16:42 +0300

    print_r debugger message if it object

--- Zetta/Log/Writers/Memory.php
+++ Zetta/Log/Writers/Memory.php
@@ -5,7 +5,7 @@ class Zetta_Log_Writers_Memory extends Zend_Log_Writer_Mock {
     protected static $Events = array();
 
     public function _write($event) {
-        self::$Events[] = $event;
+        self::$Events[] = is_string($event) ? $event : print_r($event, 1);
     }
 
     static public function getEvents() {
