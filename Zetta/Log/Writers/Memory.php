513d915068d3aed202046b412a8291c8daa8aa98
Александр Хрищанович <info@asdf.by>
2016-04-22 17:29:04 +0300

    Fix Illegal string offset 'message' error in Logger

--- Zetta/Log/Writers/Memory.php
+++ Zetta/Log/Writers/Memory.php
@@ -5,7 +5,13 @@ class Zetta_Log_Writers_Memory extends Zend_Log_Writer_Mock {
     protected static $Events = array();
 
     public function _write($event) {
-        self::$Events[] = is_string($event) ? $event : print_r($event, 1);
+
+    	if (false == is_string($event['message'])) {
+    		$event['message'] = print_r($event, 1);
+    	}
+
+        self::$Events[] = $event;
+
     }
 
     static public function getEvents() {
